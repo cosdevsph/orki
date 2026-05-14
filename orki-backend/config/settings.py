2a613ab034c8d6541a3f0b91633cab13be3c076e
Jeremiah Pantaras <jeremiahpantaras@gmail.com>
2026-05-15 06:21:38 +0800

    initial fix 2: production

--- orki-backend/config/settings.py
+++ orki-backend/config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,.onrender.com").split(",")
 
 # ─── Applications ─────────────────────────────────────────────────────────────
 

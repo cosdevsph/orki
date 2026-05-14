2fa844825dd0f37082b066bdfdc05983cec62e06
Jeremiah Pantaras <jeremiahpantaras@gmail.com>
2026-05-15 06:15:08 +0800

    initial fix: production

--- orki-backend/config/settings.py
+++ orki-backend/config/settings.py
@@ -150,6 +150,7 @@ _CORS_ORIGINS = list({
     _FRONTEND_URL,
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://orki.cosedevs.com",
 })
 
 CORS_ALLOWED_ORIGINS = _CORS_ORIGINS
